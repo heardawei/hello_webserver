8c94e6a5caba840c8a97c88ba159a7ab93384712
李大伟 <ldw01441929@alibaba-inc.com>
2022-09-28 17:37:40 +0800

    v0.0.2

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,17 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     let get = "GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(&get.as_bytes()) {
-        let html = fs::read_to_string("index.html").unwrap();
-
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            html.len(),
-            html
-        );
-        stream.write(response.as_bytes()).unwrap();
+    let (html, status_line) = if buffer.starts_with(&get.as_bytes()) {
+        ("index.html", "HTTP/1.1 200 OK\r\n")
     } else {
-        let html = fs::read_to_string("404.html").unwrap();
-        let response = format!(
-            "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}",
-            html.len(),
-            html
-        );
-        stream.write(response.as_bytes()).unwrap();
-    }
+        ("404.html", "HTTP/1.1 404 NOT FOUND\r\n")
+    };
+    let html = fs::read_to_string(html).unwrap();
+    let response = format!(
+        "{}Content-Length: {}\r\n\r\n{}",
+        status_line,
+        html.len(),
+        html
+    );
+    stream.write(response.as_bytes()).unwrap();
 }
